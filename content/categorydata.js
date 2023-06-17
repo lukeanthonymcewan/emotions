fa4222e025889b3559c2ad63c6020ca79330c7b5
lukeanthonymcewan <66076553+lukeanthonymcewan@users.noreply.github.com>
2023-06-17 17:52:29 +0200

    Add files via upload

---
+++ content/categorydata.js
@@ -0,0 +1,68 @@
+const categoryData = [
+  {
+    Title: "Big Talk",
+    Subtitle: "No small talk allowed",
+    Description:
+      "Questions designed to skip the small talk and jumpstart more curious deeper conversations.",
+    TagOne: "Identity",
+    TagTwo: "Self-reflection",
+    TagThree: "Life",
+    FloatTagOne: "Identity",
+    FloatTagTwo: "Self-reflection",
+    Order: 1,
+    PrimaryColor: "#295548",
+    SecondaryColor: "#f094e4",
+    DarkColor: "#0a2f24",
+    TextColor: "#ffffff",
+  },
+  {
+    Title: "Debatable",
+    Subtitle: "Your stance on this or that",
+    Description:
+      "Statements crafted to help you have a healthy debate on topics that you feel strongly about.",
+    TagOne: "Religion",
+    TagTwo: "Science",
+    TagThree: "Past & future",
+    FloatTagOne: "The future",
+    FloatTagTwo: "Reality",
+    Order: 2,
+    PrimaryColor: "#fc6231",
+    SecondaryColor: "#ffdc5c",
+    DarkColor: "#ce3d0f",
+    TextColor: "#ffffff",
+  },
+  {
+    Title: "The Bedroom",
+    Subtitle: "Warning: temptation ahead",
+    Description:
+      "Dive under the sheets with some intimate topics, bound to make you feel on edge",
+    TagOne: "Sex",
+    TagTwo: "Intimacy",
+    TagThree: "Sexuality",
+    FloatTagOne: "Sex",
+    FloatTagTwo: "Intimacy",
+    Order: 3,
+    PrimaryColor: "#e167a4",
+    SecondaryColor: "#c6ccf8",
+    DarkColor: "#d75093",
+    TextColor: "#ffffff",
+  },
+  {
+    Title: "Love Language",
+    Subtitle: "A chemistry check-in",
+    Description:
+      "Explore new depths in your relationship with these candid and thought-provoking conversation prompts",
+    TagOne: "Vulnerability",
+    TagTwo: "Identity",
+    TagThree: "Connection",
+    FloatTagOne: "Connection",
+    FloatTagTwo: "Dating",
+    Order: 4,
+    PrimaryColor: "#544bf5",
+    SecondaryColor: "#c7f1ff",
+    DarkColor: "#0f0d8c",
+    TextColor: "#ffffff",
+  },
+];
+
+window.categoryData = categoryData;
